5658c3039b0301dfc338cfaf919e6406cf88f494
Evgeniy Putsa <epuca@mail.ru>
2023-10-05 21:55:34 +0300

    classwork

---
+++ lesson_6/index.js
@@ -0,0 +1,45 @@
+// const string1 = 'Hello';
+// const string2 = 'Welcome';
+// const duplicatedSymbols = findDuplicates(string1, string2);
+
+// function findDuplicates(str1, str2) {
+//     str1 = str1.toLowerCase();
+//     str2 = str2.toLowerCase();
+//     let result = "";
+
+//     const [base, secondary] = str1.length < str.length ? [str1, str2] : [str2, str1]
+//     if (typeof base === 'string' && typeof base === 'string') {
+//         for (let i = 0; i < str1.length; i++) {
+//             if (str2.includes(str1[i]) && !result.includes(str1[i])) {
+//                 result += str1[i];
+//             }
+//         }
+//         return result;
+//     }
+//     return result;
+// }
+
+// console.log(duplicatedSymbols);
+
+
+// function countNumString(str) {
+//     // const a1 = str.split(""); хуже деструктуризации
+//     // const a2 = [...str];
+//     return[...str].reduce((stat, symbol) => {
+//         stat[symbol] = (stat[symbol] ?? 0) +1;
+//         return stat; 
+//     }, {});
+   
+// }
+// countNumString("dddffffds");
+
+function returnArrNum (...num){
+   const num2 = num.filter((elem) => {
+        return  elem %2 === 0;
+    })
+    return num2;
+
+}
+
+returnArrNum(1,2,3,4,5,6); 
+
