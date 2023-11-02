3a16c669494b4f4b2090d84a338f53e144f064e3
Evgeniy Putsa <epuca@mail.ru>
2023-11-02 21:56:38 +0300

    ghvhj

---
+++ lesson_13/index.js
@@ -0,0 +1,16 @@
+
+// function replaceLongColors(text){
+//     return text.replace(/#([\da-f])\1([\da-f])\2([\da-f])\3\b/gi, (_, r, g, b) => {
+//         return `#${r}${g}${b}`
+//     });
+// }
+
+// console.log(replaceLongColors('#aA55aa'));
+
+
+function replaceLongColors(text){
+    return text.replace(/_[a-z]/gi, (math) => math.charAt(1).toUpperCase())
+    
+}
+
+console.log(replaceLongColors('dfdsf_dsdf_dfsdf'));
