1a3df229546f6226187e9d173da15b706e0b9a6d
Evgeniy Putsa <epuca@mail.ru>
2023-11-09 22:28:46 +0300

    tscript

---
+++ typescript/dist/main.1fca7e6f8829655373df.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************!*\
+  !*** ./index.ts ***!
+  \******************/
+
+alert('ok');
+
+/******/ })()
+;
+//# sourceMappingURL=main.1fca7e6f8829655373df.js.map
