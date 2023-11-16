a0d5c8f6f88de07d156665e8af19a8360b8e03f1
Evgeniy Putsa <epuca@mail.ru>
2023-11-16 21:51:26 +0300

    DOM

--- typescript/src/greet.ts
+++
@@ -1,3 +0,0 @@
-export function greet(messege: string): void {
-    alert(messege);
-}
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,2 +1,26 @@
-import { greet } from "./greet";
-greet('hello word');
+const mainEl = document.getElementById('main');
+const spanColection = mainEl
+    // ? Array.from(mainEl.querySelectorAll('[data-color]') as NodeListOf<HTMLElement>)
+    ? Array.from(mainEl.querySelectorAll<HTMLElement>('[data-color]'))
+    : [];
+spanColection.forEach((spanEl) => {
+    const colorValue = spanEl.textContent;
+
+
+    function paintBG() {
+        if (colorValue) {
+            spanEl.style.backgroundColor = colorValue;
+            setTimeout(() => {
+                spanEl.style.removeProperty('background-color')
+            }, 1000);
+        }
+    }
+    spanEl.addEventListener('click', paintBG);
+    setTimeout(() => {
+        spanEl.removeEventListener('click', paintBG);
+    }, 5000)
+
+});
+
+// const bold = mainEl?.querySelector('#b');
+// bold?.addEventListener('click', () => bold.classList.add('bold')) 
