01db655b7504f5e3d414b1e20b787b2b676b939c
Evgeniy Putsa <epuca@mail.ru>
2023-11-21 21:58:16 +0300

    AddEventListener

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,26 +1,57 @@
+import { COLORS } from "./colors";
+import { createElementByColor } from "./createElementByColor";
+
 const mainEl = document.getElementById('main');
-const spanColection = mainEl
-    // ? Array.from(mainEl.querySelectorAll('[data-color]') as NodeListOf<HTMLElement>)
-    ? Array.from(mainEl.querySelectorAll<HTMLElement>('[data-color]'))
-    : [];
-spanColection.forEach((spanEl) => {
-    const colorValue = spanEl.textContent;
 
 
-    function paintBG() {
-        if (colorValue) {
-            spanEl.style.backgroundColor = colorValue;
-            setTimeout(() => {
-                spanEl.style.removeProperty('background-color')
-            }, 1000);
-        }
+const coloredElements = COLORS.map((color) => createElementByColor(color));
+//плохой вариант
+// for (const coloredElement of coloredElements){
+//     mainEl?.appendChild(coloredElement);
+// }
+
+//получше
+// mainEl?.append(...coloredElements);
+// mainEl?.insertAdjacentElement('be')
+
+// отличный, дерево обновится 1 раз только.
+
+mainEl?.appendChild(COLORS.reduce ((fragment, color) => {
+    fragment.appendChild(createElementByColor(color));
+    return fragment;
+}, document.createDocumentFragment()));
+
+
+
+mainEl?.addEventListener('click', (event) => {
+    const targetEl = event.target as HTMLElement;
+    const coloredEl = targetEl.closest<HTMLElement>('[data-color]')
+
+    if (coloredEl) {
+        const colorValue = coloredEl.getAttribute('data-color'); 
+        coloredEl.style.setProperty('background-color', colorValue)
     }
-    spanEl.addEventListener('click', paintBG);
-    setTimeout(() => {
-        spanEl.removeEventListener('click', paintBG);
-    }, 5000)
 
-});
+})
+
+
+
+
+// const spanColection = mainEl
+//     // ? Array.from(mainEl.querySelectorAll('[data-color]') as NodeListOf<HTMLElement>)
+//     ? Array.from(mainEl.querySelectorAll<HTMLElement>('[data-color]'))
+//     : [];
+// spanColection.forEach((spanEl) => {
+//     const colorValue = spanEl.textContent;
+
+
+//     function paintBG() {
+//         if (colorValue) {
+//             spanEl.style.backgroundColor = colorValue;
+//         }
+//     }
+//     spanEl.addEventListener('click', paintBG);
+// });
 
 // const bold = mainEl?.querySelector('#b');
 // bold?.addEventListener('click', () => bold.classList.add('bold')) 
